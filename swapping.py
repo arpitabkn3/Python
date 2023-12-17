d24f6e4ca7314ff7289331c42a884c96f7b410d8
Arpita Singh <151006850+arpitabkn3@users.noreply.github.com>
2023-12-18 00:44:24 +0530

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,13 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+
+
+print("enter the number",a)
+print*("enter the number",b)
+
+temp=a
+a=b
+b=temp
+
+print("after the swapping",a)
+print("after the swapping",b)
