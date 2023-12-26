377b3769e400e4b6b7600bf5494b5c9bbc60df1d
Arpita Singh <151006850+arpitabkn3@users.noreply.github.com>
2023-12-27 01:20:19 +0530

    shuffle deck card.py

---
+++ shuffle deck card.py
@@ -0,0 +1,7 @@
+import random,itertools
+deck=list(itertools.product(range(1,14),["Daimond","hearts","club","spade"]))
+random.shuffle(deck)
+print(deck)
+for i in range(5):
+    print(deck[i][0], "of",deck[i][0])
+    
