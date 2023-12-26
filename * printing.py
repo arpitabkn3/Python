b7ad702620b48a8659ac3fcbfa34af8c1a017192
Arpita Singh <151006850+arpitabkn3@users.noreply.github.com>
2023-12-27 02:17:27 +0530

    Create * printing.py

---
+++ * printing.py
@@ -0,0 +1,8 @@
+rows=int(input("enter the number in rows"))
+columns=int(input("enter the number in columns"))
+symbols=input("enter the number in symbols")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbols,end="")
+        print()
